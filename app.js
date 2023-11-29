1f8150cc7fe53dad42c24508a3fb7ae44eddc664
sabannurrahman <sabannurrahman@gmail.com>
2023-11-29 09:06:30 +0800

    instal express js dan nodemon

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var apel ="apel satu";
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello gais!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
